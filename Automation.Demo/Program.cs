fc633e6372b742963e126c852eb019a9f992aff8
shiljop <shiljo.paulson@thetrainline.com>
2018-08-19 05:18:28 +0100

    Removing unnecessary files

--- Automation.Demo/Program.cs
+++ Automation.Demo/Program.cs
@@ -29,7 +29,7 @@ namespace Automation.Demo
             {
                 Console.WriteLine("Failed");
             }
-
+            webdriver.Close();
             Console.ReadLine();
         }
     }
