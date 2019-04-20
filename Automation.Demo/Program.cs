1f8ee3ff97bfd138fdc7fd251940eef9fd2c30f3
DanBiel2019 <48848475+DanBiel2019@users.noreply.github.com>
2019-04-20 10:03:53 -0500

    Update Program.cs

--- Automation.Demo/Program.cs
+++ Automation.Demo/Program.cs
@@ -1,36 +1,82 @@
-﻿using System;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
+using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;           
+using OpenQA.Selenium.Firefox;   
+using OpenQA.Selenium.Chrome;    
+using OpenQA.Selenium.IE;
 
-namespace Automation.Demo
+namespace SeleniumBingTests
 {
-    public class Program
+  /// <summary>
+  /// Summary description for MySeleniumTests
+  /// </summary>
+  [TestClass]
+  public class MySeleniumTests
+  {
+    private TestContext testContextInstance;
+    private IWebDriver driver;
+    private string appURL;
+
+    public MySeleniumTests()
     {
-        static void Main(string[] args)
-        {
-            var webdriver = new FirefoxDriver(AppDomain.CurrentDomain.BaseDirectory);
-            webdriver.Url = @"http://bing.com";
-
-            var searchBox = webdriver.FindElement(By.Id("sb_form_q"));
-            searchBox.SendKeys("butterfly");//This method inputs butterfly into the HTML's input element
-
-            var searchButton = webdriver.FindElementById("sb_form_go");
-            searchButton.Click();
-
-            var firstResultLink = webdriver.FindElementByCssSelector("#b_results li.b_algo h2 a");
-            firstResultLink.Click();
-            var expectedUrl = "https://en.wikipedia.org/wiki/Butterfly";
-            if (expectedUrl.Equals(webdriver.Url, System.StringComparison.InvariantCultureIgnoreCase))
-            {
-                Console.WriteLine("Passed");
-            }
-            else
-            {
-                Console.WriteLine("Failed");
-            }
-            webdriver.Close();
-            Console.ReadLine();
-        }
     }
+
+    [TestMethod]
+    [TestCategory("Chrome")]
+    public void TheBingSearchTest()
+    {
+      driver.Navigate().GoToUrl(appURL + "/");
+      driver.FindElement(By.Id("sb_form_q")).SendKeys("Azure Pipelines");
+      driver.FindElement(By.Id("sb_form_go")).Click();
+      driver.FindElement(By.XPath("//ol[@id='b_results']/li/h2/a/strong[3]")).Click();
+      Assert.IsTrue(driver.Title.Contains("Azure Pipelines"), "Verified title of the page");
+    }
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    [TestInitialize()]
+    public void SetupTest()
+    {
+      appURL = "http://www.bing.com/";
+
+      string browser = "Chrome";
+      switch(browser)
+      {
+        case "Chrome":
+          driver = new ChromeDriver();
+          break;
+        case "Firefox":
+          driver = new FirefoxDriver();
+          break;
+        case "IE":
+          driver = new InternetExplorerDriver();
+          break;
+        default:
+          driver = new ChromeDriver();
+          break;
+      }
+
+    }
+
+    [TestCleanup()]
+    public void MyTestCleanup()
+    {
+      driver.Quit();
+    }
+  }
 }
